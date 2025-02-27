8b7f1ba9c6f025856112c6e472ad0aa54eb8b91d
Jordan Hiraman <jhiraman28@students.roxbury.org>
2025-02-26 20:29:57 -0500

    added gear ratio

--- src/main/java/frc/robot/subsystems/WinchSubsystem.java
+++ src/main/java/frc/robot/subsystems/WinchSubsystem.java
@@ -1,8 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.math.controller.PIDController;
-//import edu.wpi.first.wpilibj.Joystick;
-//import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -10,22 +9,25 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 
-
-
 public class WinchSubsystem extends SubsystemBase {
     private final SparkMax winchMotor;
     private final RelativeEncoder winchEncoder;
     private final PIDController winchPID;
+    private final XboxController controller;
 
-    private static final double WINCH_SPEED_MULTIPLIER = 0.8; // Adjust max speed
+    private static final double BASE_WINCH_SPEED = 0.5;
+    private double winchSpeedMultiplier = BASE_WINCH_SPEED;
+    private static final double SPEED_INCREMENT = 0.1;
     private static final double kP = 0.1;
     private static final double kI = 0.0;
     private static final double kD = 0.0;
-    private static final double TARGET_ROTATIONS = 90.0 / 360.0; // Convert degrees to rotations
+    private static final double GEAR_RATIO = 2.0; // Adjust for actual gear ratio
+    private static final double TARGET_ROTATIONS = (90.0 / 360.0) * GEAR_RATIO; 
 
     private double targetPosition = 0.0;
 
-    public WinchSubsystem() {
+    public WinchSubsystem(XboxController controller) {
+        this.controller = controller;
         winchMotor = new SparkMax(Constants.climberConstants.WINCH_MOTOR_ID, MotorType.kBrushless);
         winchEncoder = winchMotor.getEncoder();
         winchPID = new PIDController(kP, kI, kD);
@@ -33,7 +35,7 @@ public class WinchSubsystem extends SubsystemBase {
     }
 
     public void setWinchSpeed(double speed) {
-        winchMotor.set(speed * WINCH_SPEED_MULTIPLIER);
+        winchMotor.set(speed * winchSpeedMultiplier);
     }
 
     public void moveWinchTo90Degrees() {
@@ -44,5 +46,14 @@ public class WinchSubsystem extends SubsystemBase {
 
     public void stopWinch() {
         winchMotor.set(0);
+    }
+
+    @Override
+    public void periodic() {
+        if (controller.getRawButtonPressed(5)) { // Left D-pad (increase speed)
+            winchSpeedMultiplier = Math.min(winchSpeedMultiplier + SPEED_INCREMENT, 1.0);
+        } else if (controller.getRawButtonPressed(6)) { // Right D-pad (decrease speed)
+            winchSpeedMultiplier = Math.max(winchSpeedMultiplier - SPEED_INCREMENT, 0.1);
+        }
     }
 }
